58a5e20f787577608e72b0682d58e7634c1e0547
pank1999 <pankajneeraj541@gmail.com>
2023-01-02 22:47:44 +0530

    [course-dashboard]Implemented courseDashboard func

--- frontend/src/app/learning/learning-routing.module.ts
+++ frontend/src/app/learning/learning-routing.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { CourseDashboardComponent } from './course-dashboard/course-dashboard.component';
+import { VideoComponent } from './video/video.component';
 
 const routes: Routes = [
   {
-    path: 'learning/dashboard/:courseName',
+    path: 'learning/dashboard/:course',
     component: CourseDashboardComponent,
+    children: [
+      {
+        path: 'video/:subTopicsId',
+        component: VideoComponent,
+      },
+    ],
   },
 ];
 
